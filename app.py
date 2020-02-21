17ef14896c0e57e92df5d8c8b08c61be73d9bbf7
Ethan Jones <joneetha@oregonstate.edu>
2020-02-20 16:31:23 -0800

    stub out ddb functionality

--- app.py
+++ app.py
@@ -1,36 +1,36 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 import boto3
 import json
 
-BUCKET_NAME = 'cs493-joneetha-music'
-
 app = Flask(__name__)
 
-@app.route('/')
-def serve():
-  resp = Response(json.dumps(get_music_obj()))
-  resp.headers['Access-Control-Allow-Origin'] = '*'
-  return resp
+@app.route('/songs/for/album')
+def songs_for_album():
+  return respond('album')
 
-def get_music_obj():
-  session = boto3.Session()
-  s3 = session.resource('s3')
-  res = {}
-  for o in s3.Bucket(BUCKET_NAME).objects.all():
-    path = o.key.split('/')
-    artist = path[0]
-    album = path[1]
-    title = path[2]
-    presigned = boto3.client('s3').generate_presigned_url(
-      'get_object',
-      Params={'Bucket': BUCKET_NAME, 'Key': o.key},
-      ExpiresIn=3600
-    )
-    if artist in res:
-      if album in res[artist]:
-        res[artist][album][title] = presigned
-      else:
-        res[artist][album] = { title: presigned }
-    else:
-      res[artist] = { album: { title: presigned } }
-  return res
+@app.route('/albums/for/artist')
+def albums_for_artist():
+  return respond('artist')
+
+@app.route('/artists/for/genre')
+def artists_for_genre():
+  return respond('genre')
+
+def respond(arg):
+  arg = request.args.get(arg, '')
+  response = Response(lookup_ddb(arg))
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  return response
+
+def lookup_ddb(key):
+  ddb = boto3.client('dynamodb')
+  response = ddb.query(
+    TableName='music',
+    KeyConditionExpression='PK = :v1',
+    ExpressionAttributeValues={
+      ':v1': {
+        'S': key
+      },
+    }
+  )
+  return str(response)
